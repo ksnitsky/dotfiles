4ff27e73755df0718210b56dd2a6c19ec3a006fc
ksnitsky <ksnitsky@gmail.com>
2023-09-15 21:55:07 +0600

    waybar: minor changes

--- waybar/powermenu.sh
+++ waybar/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -i --dmenu | awk '{print tolower($2)}' )
+op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | tofi | awk '{print tolower($2)}' )
 
 case $op in
         poweroff)
