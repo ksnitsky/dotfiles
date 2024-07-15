d39208e43905fcdfdebb14498b25ad1fdef0567d
ksnitsky <ksnitsky@gmail.com>
2024-07-15 18:58:39 +0600

    nvim: added git-dev and updated which-key

--- nvim/lua/kickstart/plugins/which-key.lua
+++ nvim/lua/kickstart/plugins/which-key.lua
@@ -20,23 +20,31 @@ return {
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        -- ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it Hunk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        {
+          mode = 'n',
+          { "<leader>b",  group = "[B]uffer" },
+          { "<leader>b_", hidden = true },
+          { "<leader>c",  group = "[C]ode" },
+          { "<leader>c_", hidden = true },
+          { "<leader>d",  group = "[D]ocument" },
+          { "<leader>d_", hidden = true },
+          { "<leader>g",  group = "[G]it Hunk" },
+          { "<leader>g_", hidden = true },
+          { "<leader>r",  group = "[R]ename" },
+          { "<leader>r_", hidden = true },
+          { "<leader>s",  group = "[S]earch" },
+          { "<leader>s_", hidden = true },
+          { "<leader>t",  group = "[T]oggle" },
+          { "<leader>t_", hidden = true },
+          { "<leader>w",  group = "[W]orkspace" },
+          { "<leader>w_", hidden = true },
+        },
+        {
+          mode = 'v',
+          { "<leader>g", desc = "[G]it Hunk" },
+        }
       }
-      -- visual mode
-      require('which-key').register({
-        -- ['<leader>h'] = { 'Git [H]unk' },
-        ['<leader>g'] = { '[G]it Hunk' },
-      }, { mode = 'v' })
     end,
   },
 }
